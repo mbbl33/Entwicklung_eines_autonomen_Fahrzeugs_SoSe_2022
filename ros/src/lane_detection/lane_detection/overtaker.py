e08d31aa44ccdb2040a811ebed358fbe391a3331
Autonomes Fahren <sven.thomas@mni.thm.de>
2022-06-23 10:32:18 +0200

    improved overtaker, removed hardcoded speed and added roi switch

--- ros/src/lane_detection/lane_detection/overtaker.py
+++ ros/src/lane_detection/lane_detection/overtaker.py
@@ -4,6 +4,7 @@ from sensor_msgs.msg import Range
 from std_msgs.msg import Float64, Bool, Int64MultiArray
 from . import pid
 import time
+import math
 
 
 class Overtaker(Node):
@@ -20,25 +21,21 @@ class Overtaker(Node):
         self.subLeft = self.create_subscription(Range, 'tof_Front_Left', self.look_left, 1)
         self.subRight = self.create_subscription(Range, 'tof_Front_Right', self.look_right, 1)
 
+        self.subSpeed = self.create_subscription(Float64, '/speed', self.get_current_speed, 1)
+        self.speed = 0
+        self.lc_time = 0
+
         self.right_dist = 0.0
         self.pid_controller = pid.PID_Controller(100, 0.001, 1, 45, -45, (1 / 5))
         
-        # speed sub
-        #self.sub_speed = self.create_subscription(Float64, '/speed', self.overtake, 1)
-
         # steering
         self.pub_steering = self.create_publisher(Float64, '/steering', 1)
-        self.pub_speed = self.create_publisher(Float64, '/speed', 1)
 
         self.overtaker_mode = False
         self.left_lane_free = True
         self.right_lane_free = True
 
         self.current_phase = 0
-
-        #self.start_time = 0.0
-
-        #self.delay = 6.5
 
     def is_road_blocked(self, msg):
         range = min(msg.max_range, msg.range)
@@ -62,14 +59,13 @@ class Overtaker(Node):
         if self.overtaker_mode == True:
             if self.current_phase == 1:
                 out = Float64()
-                out.data = 0.2
-                self.pub_speed.publish(out) # SUBJECT TO CHANGE - Hardcoded speed
-                out.data = -15.0
+                out.data = -25.0
                 self.pub_steering.publish(out)
-                time.sleep(3.33) # SUBJECT TO CHANGE - Hardcoded Speed
-                out.data = 15.0
+                self.lc_time = self.calc_lc_time()
+                time.sleep(self.lc_time * 0.9)
+                out.data = 25.0
                 self.pub_steering.publish(out)
-                time.sleep(3.33) # SUBJECT TO CHANGE - Hardcoded Speed
+                time.sleep(self.lc_time * 0.9)
                 out.data = 0.0
                 self.pub_steering.publish(out)
                 self.current_phase = 2
@@ -77,14 +73,14 @@ class Overtaker(Node):
             # Phase 2 - Switch RoI and Hold Lane
             elif self.current_phase == 2:
                 new_roi_msg = Int64MultiArray()
-                new_roi_msg.data = [ 280, 500, 100, 40 ]
+                new_roi_msg.data = [ 260, 500, 100, 40 ]
                 new_factor_msg = Float64()
-                new_factor_msg.data = 0.75
+                new_factor_msg.data = 0.85
                 self.pub_adjust_RoI.publish(new_roi_msg)
                 self.pub_adjust_factor.publish(new_factor_msg)
                 msg_ld = Bool()
                 msg_ld.data = False
-                #self.pub_update_LD.publish(msg_ld) SUBJECT TO CHANGE - Switch Lane_Detection
+                self.pub_update_LD.publish(msg_ld)
                 if self.right_lane_free == False:
                     self.current_phase = 3
                     print("---SET CURRENT PHASE TO 3---")
@@ -109,12 +105,13 @@ class Overtaker(Node):
             # Phase 5 - Switch Lanes back
             elif self.current_phase == 5:
                 out = Float64()
-                out.data = 15.0
+                out.data = 25.0
                 self.pub_steering.publish(out)
-                time.sleep(3.33)
-                out.data = -15.0
+                self.lc_time = self.calc_lc_time()
+                time.sleep(self.lc_time * 0.9)
+                out.data = -25.0
                 self.pub_steering.publish(out)
-                time.sleep(3.33)
+                time.sleep(self.lc_time * 0.9)
                 out.data = 0.0
                 self.pub_steering.publish(out)
                 msg_ld = Bool()
@@ -125,6 +122,16 @@ class Overtaker(Node):
             else:
                 return
 
+    def calc_lc_time(self):
+        gamma = 1.57079 # 90 degrees in rad
+        beta = 0.43633 # 25 degrees in rad
+        b = 0.25
+        c = (b * math.sin(gamma)) / math.sin(beta) # calculation of effective distance via sine rule
+        time = c / self.speed # calculation of needed time via distance / speed
+        return time
+
+    def get_current_speed(self, msg):
+        self.speed = msg.data
 
     def look_left(self, msg):
         self.left_lane_free = 0.5 < msg.range
